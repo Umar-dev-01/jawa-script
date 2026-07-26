1e709561ade00fec96647962b9d4e26ffc0732a3
Umar-dev-01 <01.dev.umar@gmail.com>
2026-07-26 16:46:03 +0500

    shk

--- 02_basic/functions.js
+++ 02_basic/functions.js
@@ -29,8 +29,58 @@ function showAge(age){
     console.log(age);
 }
 showAge(18)
-*/
+
 
 function showCity(city){
-    
+console.log(city)
 }
+showCity("Lahore")
+
+
+function greet(name){
+    console.log("Hello "+name);
+}
+greet("Ali")
+
+
+
+function add(a,b){
+    console.log(a+b);
+}
+add(10,15)
+
+function multi(a, b){
+    console.log(a*b);
+}
+multi(6,7)
+
+
+function introduce(name, age){
+    console.log("My Name is "+(name)+" and I am "+(20)+" years old.");
+}
+introduce ("Umar", 20)
+
+
+
+function square(num){
+    return num*num;
+}
+let ans = square(5);
+console.log(ans)
+
+
+
+function cube(num){
+   return num*num*num;
+}
+let result= cube(3);
+console.log(result)
+
+
+function fullName(firstName, lastname){
+return (firstName+lastname);
+}
+let name = fullName("Muhammad ", "Umar");
+console.log(name);
+
+*/
