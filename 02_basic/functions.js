c96417021c11ed36ee8b014da7653a8bc15719f6
Umar-dev-01 <01.dev.umar@gmail.com>
2026-07-26 15:39:44 +0500

    moskl

---
+++ 02_basic/functions.js
@@ -0,0 +1,36 @@
+/*
+function sayHello(){
+    console.log("Hello Umar");
+}
+sayHello()
+
+function showName(name) {
+    console.log(name)
+}
+showName("Muhammad Umar")
+
+
+
+function printCity(){
+    console.log("Rawalpindi");
+}
+printCity()
+
+
+
+function printCountry(){
+    console.log("Pakistan");
+}
+printCountry()
+printCountry()
+
+
+function showAge(age){
+    console.log(age);
+}
+showAge(18)
+*/
+
+function showCity(city){
+    
+}
